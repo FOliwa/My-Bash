478df17d30f774418e2f93707f7479119ab787f8
FOliwa <filip.oliwa@gmail.com>
2023-09-20 23:34:10 +0200

    Add backuping script

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # My-Bash
+This is my bash playgroun created for lerning purpouses. As at dayli buiasis I have to work from time to time with BASH I decided to train a bit BASH scripting. This repo is totally for fun and lerning purpouses - in this order.
+
+
+# Scripts
+This repo contain a **scripts** folder where I will put all scripts and data for tests. I 
+
+## 1. Backup
+This is simple backup script for important files.
+
+### Objectives:
+Create a Bash script that automates the process of backing up important files and directories to a specified backup location.
+
+## Requirements
+backup.sh
+- [x] Create a timestamp-based directory inside the backup location
+- [x] Copy all files and subdirectories from the important_files directory to the new backup directory.
+- [x] Log the backup process, including the timestamp and the list of files copied, to a log file.
+- [ ] Handle errors gracefully (e.g., if the backup location doesn't exist or if there are permission issues).
---
+++ scripts/backup/backup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#-------------------------------------------------- 
+# Change below variables to indicate the place 
+# you want to backup and where you wnat to store it.
+
+BACKUP_THIS="./tests/important_files/"
+SAVE_BACKUP_HERE="./tests/backuped_files"
+
+#---------------------------------------------------
+
+echo "========== START BACKUP SCRIPT ==========="
+
+# Create timestamped name for new backup directory
+timestamp=$(date "+%Y%m%d")
+LOG_FILE_NAME="backup.log"
+BACK_DIR_PATH="$SAVE_BACKUP_HERE/backup_$timestamp"
+LOG_FILE_PATH="$BACK_DIR_PATH/$LOG_FILE_NAME"
+
+# Check if directory exist before create
+if [ -d "$BACK_DIR_PATH" ]
+then
+    echo "[INFO] Directory $BACK_DIR_PATH already exists." >> $LOG_FILE_PATH
+else 
+    mkdir $BACK_DIR_PATH
+    echo "[INFO] Directory $BACK_DIR_PATH created." >> $LOG_FILE_PATH
+fi
+
+# Copy all files and subdirectories to the new backup directory.
+echo "[INFO] File copying has started: $(date "+%Y%m%d %H:%M:%S")" >> $LOG_FILE_PATH
+echo "From: $BACKUP_THIS"
+echo "To:   $BACK_DIR_PATH"
+
+# -R = recursive copying
+# -v = print out each file name as it copies
+# -u = copy only when the source file is newer than the destination file
+# 2>&1 = redirect STDOUT and STDERROR to the LOG file
+cp -Rvn $BACKUP_THIS $BACK_DIR_PATH >> $LOG_FILE_PATH 2>&1
+
+echo "[INFO] Copy completed at: $(date "+%Y%m%d %H:%M:%S")" >> $LOG_FILE_PATH
