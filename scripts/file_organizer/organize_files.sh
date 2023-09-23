149eb86e5de3b516409586d73eebecf7d71a6ee0
FOliwa <filip.oliwa@gmail.com>
2023-09-24 00:07:24 +0200

    Organising script fixed for files with no extension

--- scripts/file_organizer/organize_files.sh
+++ scripts/file_organizer/organize_files.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
-# BASH NOTES:
-# [SYNTAX] - "${!#}" is used to access the argument at the last position
-# [SYNTAX] - "$#" represents the number of arguments passed to the script
+# Script flags ----------------------------------------------------------
+# -c = [DEFAULT MODE] copy files to the new location
+# -m = move files to the new locationt
 
+
+# ----------------  FUNCTIONS -------------------------------------------
+function organise_files() {
+    for extension in $(find $dir_to_organize -maxdepth 1 -type f -name "*.*" | rev | cut -d . -f 1 | rev | sort -u) 
+    do
+        echo "  -> [ ] $extension - Processing..."
+        mkdir -p "$dir_to_organize/$extension"
+        # INFO: I redirect to /dev/null as I dont want to spam user
+        if [ "$organising_mod" = "move" ]; then
+            mv $dir_to_organize/*.$extension $dir_to_organize/$extension
+        else
+            cp $dir_to_organize/*.$extension $dir_to_organize/$extension
+        fi
+        echo "  -> [X] $extension - Done!"
+    done
+}
+
+
+# ------------------- MAIN --------------------------------------------------
 echo '================ ORGANISING "THINGS" STARTED =========================='
-
 if [ $# -gt 0 ]; then
     dir_to_organize="${!#}"
     if [ -d "$dir_to_organize" ]; then
@@ -17,32 +35,18 @@ else
     echo "[INFO] Missing argument: provided path to the directory you would like to organise."
 fi
 
-# Flags:
-# -c = [DEFAULT MODE] copy files to the new location
-# -m = move files to the new locationt
 organising_mod="copy"
-
 if [ "$1" = "-m" ]; then
     organising_mod="move"
 fi
 
 dir_to_organize="${!#}"
-if find $dir_to_organize -maxdepth 1 -type f | rev | cut -d . -f 1 | grep -q $dir_to_organize; then
+if find $dir_to_organize -maxdepth 1 -type f -name "*.*" | grep -q .; then
     echo "[INFO] Files with extensions found!"
+    organise_files
 else
     echo "[INFO] There are no files with extensions..."
 fi
 
-for extension in $(find $dir_to_organize -maxdepth 1 -type f | rev | cut -d . -f 1 | rev | sort -u) 
-do
-    echo "  -> [ ] $extension - Processing..."
-    mkdir -p "$dir_to_organize/$extension"
-    # INFO: I redirect to /dev/null as I dont want to spam user
-    if [ "$organising_mod" = "move" ]; then
-        mv $dir_to_organize/*.$extension $dir_to_organize/$extension
-    else
-        cp $dir_to_organize/*.$extension $dir_to_organize/$extension
-    fi
-    echo "  -> [X] $extension - Done!"
-done
+
 echo '================ DONE!!!! =========================='
