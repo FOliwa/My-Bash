4d0489a0432f78d4f8ad7bd7d268790b3557681b
FOliwa <filip.oliwa@gmail.com>
2023-10-02 23:17:33 +0200

    Runnig bash function remotely over ssh tested and works

--- system_hardener/harden_system.sh
+++ system_hardener/harden_system.sh
@@ -3,19 +3,25 @@
 #================== CONFIGURATION ====================
 # Define SSH connectio variables
 
-# Put here your remote server IP address
-remote_server=10.0.2.15
-# Your SSH username
+# For test purpouses I created Ubuntu VM
+# In your case you have to configure the script  
+# for your needs.
+remote_server=192.168.1.147
 ssh_user=oliwa
-# Puy SSH kye path here
 ssh_key=/home/oliwa/.ssh/id_rsa.pub
 
 #==================== FUNCTIONS ======================
 
 function connection_test() {
-    echo "Test MSG $(date)" >> connection_test.txt
+    echo "$(date) - [TEST] Executing bash function remotelly over ssh." >> connection_test.txt
 }
 
 #================== SSH CONNECTION ====================
-# SSH into the remote server and execute a function
-ssh -i "$ssh_key" "$ssh_user@$remote_server" 
+# NOTE:
+# - Run functions on a remote server using SSH
+# - connection_test is a function defined in the script
+# - to run function remotely using ssh I have to retrieve 
+#   the function definition and then call I can call it to execute remotelly.
+#   declare -f some_function allowe me to retrieve function definition.
+
+ssh -i "$ssh_key" "$ssh_user@$remote_server" "$(declare -f connection_test); connection_test"
