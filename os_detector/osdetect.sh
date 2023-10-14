b2fd1fbf5b8bf56b0682b9fbfefa5a17e6dc4b6d
FOliwa <filip.oliwa@gmail.com>
2023-10-14 23:32:18 +0200

    Minor changes - comments edited

--- os_detector/osdetect.sh
+++ os_detector/osdetect.sh
@@ -4,9 +4,11 @@
 
 if type -t wevtutil &> /dev/null
 then
+    # wevtutil is utility to get info about event logs on linux
     OS=MSWindows
 elif type -t scutil &> /dev/null
 then
+    # scutil is basic command on MacOS for displaying basic network information
     OS=MacOS
 else
     OS=Linux
